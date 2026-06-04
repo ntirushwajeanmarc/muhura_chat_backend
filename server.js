60feccb02adf9311b73c353d164bf55daa56f805
ntirushwajeanmarc <ntirushwajeanmarc@gmail.com>
2026-06-04 14:42:24 +0200

    first commit

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   'https://muhura-chat-frontend.onrender.com',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'http://localhost:5173',
-  'http://127.0.0.1:5173',
-  'http://localhost:4000',
-  'http://127.0.0.1:4000',
+
 ];
 
 const corsOptions = {
